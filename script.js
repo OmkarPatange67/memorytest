14a1e77c438c3cfb28c88040a3d0cdd90df2c244
Omkar Patange <omkarpatange2002@gmail.com>
2019-04-25 17:31:26 +0530

    letters removed
    letters removed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L','M','M','N','N','O','O','P','P','Q','Q','R','R','S','S','T','T','U','U','V','V','W','W','X','X'];
+var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
 	var memory_values = [];
 	var memory_tile_ids = [];
 	var tiles_flipped = 0;
